b20fbf398bf44ca29cec798bffd56b966c128e76
vpayes <vanesa_payes@outlook.com>
2024-11-24 21:19:04 -0500

    Setup root component with global state and routing.

---
+++ App.jsx
@@ -0,0 +1,14 @@
+// Task 1 
+import React from 'react';
+import Gallery from './Gallery';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="app-title">Tour Comparison</h1>
+      <Gallery />
+    </div>
+  );
+};
+
+export default App;
